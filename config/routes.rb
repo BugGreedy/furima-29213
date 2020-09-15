44b63a40ee8831ea478cddaa6c0b64cea5a3c2db
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-09-15 12:34:27 +0900

    push

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
-  resources :users
+  resources :users, only: :show
 
   post '/items/new' => 'items#create'
 
