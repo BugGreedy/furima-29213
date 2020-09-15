4407af7381a4c377f7f42b858333c48f69b561bc
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-09-15 17:39:01 +0900

    heroku_3

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     text                    { Faker::Lorem.sentence }
     name                    { Faker::Games::Pokemon.name }
     condition_id            { Faker::Number.between(from: 1, to: 6) }
-    genre_id                {Faker::Number.between(from: 1, to: 10)}
+    genre_id                { Faker::Number.between(from: 1, to: 10)}
     shipping_charge_id      {Faker::Number.between(from: 1, to: 2)}
     shipping_place_id       {Faker::Number.between(from: 1, to: 10)}
     shipping_date_id        {Faker::Number.between(from: 1, to: 47)}
