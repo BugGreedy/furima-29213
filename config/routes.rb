e1275c0be879638afe90313a5b0f9f3d33d74e62
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-10-06 09:20:22 +0900

    items/index検索フォームのビューを調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users
   
